ab5c0a2db400124c1a4d5ebcd202416200f6dcb5
mazarafa <arruda.rafa.ra@gmail.com>
2020-06-19 17:52:29 -0400

    adicionando calendario em portugues, falta vincular dadas informativas como feriados + eventos da UFFS

--- src/app/calendario/calendario.module.ts
+++ src/app/calendario/calendario.module.ts
@@ -7,14 +7,27 @@ import { IonicModule } from '@ionic/angular';
 import { CalendarioPageRoutingModule } from './calendario-routing.module';
 
 import { CalendarioPage } from './calendario.page';
+import { CalendarModule  } from 'ion2-calendar';
+import {  LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localept from '@angular/common/locales/pt'
+
+
+
+registerLocaleData(localept, 'pt');
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CalendarioPageRoutingModule
+    CalendarioPageRoutingModule,
+    CalendarModule,
+    
+    
   ],
-  declarations: [CalendarioPage]
+  declarations: [CalendarioPage],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }]
+  // providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
 })
 export class CalendarioPageModule {}
--- src/app/calendario/calendario.page.ts
+++ src/app/calendario/calendario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarComponentOptions } from 'ion2-calendar'
+
 
 @Component({
   selector: 'app-calendario',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarioPage implements OnInit {
 
+  dateRange: { from: string; to: string; };
+  type: 'string';
+  
+  optionsRange: CalendarComponentOptions = {
+    monthFormat: this.type,
+    weekdays: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
+    weekStart: 1,
+    //defaultDate: new Date()
+  };
+
+
   constructor() { }
 
   ngOnInit() {
   }
+  
 
 }
